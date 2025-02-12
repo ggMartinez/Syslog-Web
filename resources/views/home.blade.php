afc0be04f5433f62879aad7d52c8b64f07d23eac
Gonzalo Martinez <gonzalo.martinez@litermi.com>
2025-02-12 03:24:20 -0300

    Added count number

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,6 +16,16 @@
         <h3>Logged events for host "{{ Request::get('host') }}", tagged as "{{ Request::get('tag') }}"</h3>
     @endif
    
+    
+    @if($Events -> total() > 0)
+        <!-- allign the text to the right -->
+        <div class="text-end">
+            Showning <b>{{ number_format($Events -> firstItem(), 0, ',', '.') }}</b> to <b>{{ number_format($Events -> lastItem(), 0, ',', '.') }}</b> of <b>{{ number_format($Events -> total(), 0, ',', '.') }}</b> events 
+
+
+        </div>
+    @endif
+
     @include("components.eventList")
 
 
