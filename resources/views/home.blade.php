6e065d0b6472bb391c23f0e47f9c8dc8eebee5a2
Gonzalo Martinez <gonzalo.martinez@litermi.com>
2025-02-12 02:55:27 -0300

    Fancy home title

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,21 @@
 @include('layouts.header')
     
-    @if(is_null(Request::get('host')))
-        <h3>All Logged events</h3>
-    @else 
-        <h3>"{{ Request::get('host')}}" Logged events</h3>
+    @if(is_null(Request::get('host')) && is_null(Request::get('tag')))
+        <h3>Logged events for all Hosts</h3>
     @endif
 
+    @if(is_null(Request::get('host')) && !is_null(Request::get('tag')))
+        <h3>Logged events for all Hosts, tagged as "{{ Request::get('tag') }}"</h3>
+    @endif
+
+    @if(is_null(Request::get('tag')) && !is_null(Request::get('host')))
+        <h3>Logged events for host "{{ Request::get('host') }}"</h3>
+    @endif
+
+    @if(!is_null(Request::get('host')) && !is_null(Request::get('tag')))
+        <h3>Logged events for host "{{ Request::get('host') }}", tagged as "{{ Request::get('tag') }}"</h3>
+    @endif
+   
     @include("components.eventList")
 
 
